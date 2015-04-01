d161ce66cdd21172c217e6cec9b4e8ed72c106c8
James Ward <james@jamesward.org>
2015-04-01 17:11:02 -0600

    get the tasks as JSON

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,4 +18,9 @@ public class Application extends Controller {
         return redirect(routes.Application.index());
     }
 
+    public static Result getTasks() {
+        java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
+        return ok(play.libs.Json.toJson(tasks));
+    }
+
 }
