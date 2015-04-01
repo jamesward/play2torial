d879e60945dc6e21b01f4c03d94d5eb3c7be0550
James Ward <james@jamesward.org>
2015-04-01 16:23:25 -0600

    added Application controller test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -41,4 +41,13 @@ public class ApplicationTest {
         assertThat(contentAsString(html)).contains("hello, world");
     }
 
+    @Test
+    public void testIndex() {
+        Result result = controllers.Application.index();
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("text/html");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(contentAsString(result)).contains("hello, world");
+    }
+
 }
